0ac0345492c4eff986056d380d7a117d988914ae
danzalux <michael.sopunov@gmail.com>
2025-08-12 16:54:54 +0200

    Updated "Confirm-find" for August 2025 NoIP-changes

--- renew.py
+++ renew.py
@@ -9,10 +9,17 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 LOGIN_URL = "https://www.noip.com/login?ref_url=console"
-MY_NOIP_URL = "https://my.noip.com/dynamic-dns"
+MY_NOIP_URL = "https://my.noip.com/dns/records"
 LOGOUT_URL = "https://my.noip.com/logout"
 LOGOUT_PATH = "/logout"
 
+
+def showTimeDiff(on_off, tme_bk):
+    if on_off:
+        time_diff = datetime.now() - tme_bk
+        tme_bk = datetime.now()
+        print("> Time_diff: " + time_diff.__str__() + " ms")
+        return tme_bk
 
 def doTraceLog(on_off, str_string):
     if on_off:
@@ -24,13 +31,17 @@ def doTraceLog(on_off, str_string):
 #        .find_element(By.XPATH, "/html/body/div[1]/div/div/div[3]/div[1]/div[2]/div/div/div[1]/div[1]/table/tbody") \
 #        .find_elements(By.TAG_NAME, "tr")
 
-def findHosts_method2(browser):
-    return browser \
-        .find_element(By.ID, "host-panel") \
-        .find_element(By.TAG_NAME, "table") \
-        .find_element(By.TAG_NAME, "tbody") \
-        .find_elements(By.TAG_NAME, "tr")
+# def findHosts_method2(browser):
+#     return browser \
+#         .find_element(By.ID, "host-panel") \
+#         .find_element(By.TAG_NAME, "table") \
+#         .find_element(By.TAG_NAME, "tbody") \
+#         .find_elements(By.TAG_NAME, "tr")
 
+def findConfirmButtons(browser):
+    return browser \
+        .find_element(By.ID, "zone-collection-wrapper") \
+        .find_elements(By.TAG_NAME, "button")
 
 def get_user_agent():
     return "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
@@ -94,12 +105,12 @@ def main():
     if len(argv) > 3:  # any value for debug-TraceLog-on/off supplied?
         trace_on_off = argv[3]
 
-    doTraceLog(trace_on_off, argv[0])
+    doTraceLog(trace_on_off, ">Executing: " + argv[0])
 
     # OPEN BROWSER
     print("-----------------------------------------------")
-    now = datetime.now()
-    print(now, "  \nOpening browser (wait 5 sec plz)...\n")
+    time_bak = datetime.now()
+    print(time_bak, "  \nOpening browser (wait 5-15 sec plz)...\n")
     browser_options = webdriver.FirefoxOptions()
     browser_options.add_argument("--headless")
     browser_options.add_argument("--no-sandbox")
@@ -110,11 +121,16 @@ def main():
     browser = webdriver.Firefox(options=browser_options)
 
     # LOGIN
-    print("Opening login-url (wait 7 sec)... ")
+    # time_diff = datetime.now() - time_bak
+    # time_bak = datetime.now()
+    # doTraceLog(trace_on_off, ">Time_diff: " + time_diff.__str__() + " ms")
+    time_bak = showTimeDiff(trace_on_off, time_bak)
+    doTraceLog(trace_on_off, "Opening login-url (wait 7-15 sec)... ")
     browser.get(LOGIN_URL)
 
     if browser.current_url == LOGIN_URL:
-        doTraceLog(trace_on_off, "...entering noip-login credentials...\n")
+        time_bak = showTimeDiff(trace_on_off, time_bak)
+        doTraceLog(trace_on_off, "> ...entering noip-login credentials...\n")
         browser.find_element(By.NAME, "username").send_keys(email)
         field_pwd = browser.find_element(By.NAME, "password")
         field_pwd.send_keys(password)
@@ -129,9 +145,10 @@ def main():
                     or button.text == "Entrar" \
                     or button.text == "Accedi" \
                     or button.text == "Iniciar Sesión":
-                doTraceLog(trace_on_off, "Found button: '" + button.text + "', Waiting for Click...")
+                doTraceLog(trace_on_off, "> Found button: '" + button.text + "', Waiting for Click-response...")
                 button.click()
-                doTraceLog(trace_on_off, "Clicked 'Log In' button")
+                time_bak = showTimeDiff(trace_on_off, time_bak)
+                doTraceLog(trace_on_off, "> Clicked 'Log In' button")
                 login_status = True
                 break
 
@@ -139,61 +156,41 @@ def main():
             print("Login button has changed. Please check script or contact support. ")
             exit(1)
 
-        sleep(0.100)
-
         # print(browser.get_full_page_screenshot_as_base64())
         if str(browser.current_url).endswith("noip.com/"):
-
-            print("Login successful")
-            doTraceLog(trace_on_off, str(browser.current_url) + ' |' + browser.title)
-            doTraceLog(trace_on_off, "\nFetching Hosts-Url:" + MY_NOIP_URL + " ...")
+            if not trace_on_off:
+                print("Login successful.")
+            doTraceLog(trace_on_off, "Login successful. Page-url is:")
+            doTraceLog(trace_on_off, str("> " + browser.current_url) + ' |' + browser.title)
+            doTraceLog(trace_on_off, "\n> Fetching Hosts-Url:" + MY_NOIP_URL + " ...")
             browser.get(MY_NOIP_URL)
-            sleep(0.100)
+            sleep(0.050)
 
             aux = 1
             while not browser.title.startswith("My No-IP") and aux < 4:
                 print("check BrowserTitle:" + browser.title + " Attempt:" + str(aux))
                 browser.get(MY_NOIP_URL)
-                sleep(0.250)
+                sleep(0.150)
                 aux += 1
 
             if browser.title.startswith("My No-IP") and aux < 5:
-                doTraceLog(trace_on_off, "Success with:" + str(browser.current_url) + ' |' + browser.title)
+                time_bak = showTimeDiff(trace_on_off, time_bak)
+                doTraceLog(trace_on_off, "> Success with:" + str(browser.current_url) + ' |' + browser.title)
                 confirmed_hosts = 0
 
                 # RENEW HOSTS
                 try:
-                    hosts = findHosts_method2(browser)
-                    doTraceLog(trace_on_off, "\nLooking for Hosts:")
+                    doTraceLog(trace_on_off, "\n> Looking for Buttons in Wrapper:")
+                    buttons = findConfirmButtons(browser)
 
-                    for host in hosts:
-                        print("Details for: " + host.find_element(
-                            By.TAG_NAME, "td").find_element(
-                            By.CLASS_NAME, "link-info").text)
-                        print("-------------------")
-
-                        for td in browser.find_elements(By.TAG_NAME, "td"):
-                            doTraceLog(trace_on_off, td.text)
-                            if "IP / Target" == td.get_attribute("data-title"):
-                                # not needed all other info's
-                                break
-                        doTraceLog(trace_on_off, "-------------------")
-
+                    doTraceLog(trace_on_off, "\n> Iterating Buttons:")
+                    for button in buttons:
                         try:
-                            days_label = host.find_element(By.CLASS_NAME, "no-link-style")
-                            print("Host_Label: " + days_label.text)
-                            if days_label.get_attribute("data-original-title"):
-                                print(days_label.get_attribute("data-original-title"))
+                            doTraceLog(trace_on_off, "Button: " + button.text)
 
                         except NoSuchElementException:
-                            print("'Days_label' not found")
-
-                        try:
-                            button = host.find_element(By.TAG_NAME, "button")
-                            doTraceLog(trace_on_off, "Found Host-Button: " + button.text)
-                        except NoSuchElementException:
-                            print("Host-button not found")
-                            break
+                            doTraceLog(trace_on_off, "> 'ERROR: Zero(0) Buttons on web-page in Wrapper found")
+                            continue
 
                         if button.text == "Confirm" \
                                 or button.text == "Bestätigen" \
@@ -212,7 +209,7 @@ def main():
                                     print("Click: " + button.text)
                                     button.click()
                                     print("Waiting for Captcha (not implemented yet)...")
-                                    print("...Just please check and confirm hosts manually, when errors appear.")
+                                    print("...Just please logIn, check and confirm hosts manually, when errors appear.")
                                     print("-------------------")
                                     # Doku: after unconfirmed expiration period host become "blocked" status",
                                     # and you need reset/confirm it unlikely with Captcha
@@ -220,9 +217,9 @@ def main():
 
                             # browser.get_full_page_screenshot_as_file('03.png')
                             confirmed_hosts += 1
-                            confirmed_host = host.find_element(By.TAG_NAME, "a").text
+                            confirmed_host = browser.find_element(By.ID, "zone-collection-wrapper").find_element(By.TAG_NAME, "h4").text
                             print("\nHost \"" + confirmed_host + "\" confirmed")
-                            sleep(0.100)
+                            sleep(0.050)
 
                         print("===================")
 
@@ -244,7 +241,8 @@ def main():
                     else:
                         print(str(confirmed_hosts) + " hosts confirmed\n")
 
-                    doTraceLog(trace_on_off, "Finished with hosts")
+                    time_bak = showTimeDiff(trace_on_off, time_bak)
+                    doTraceLog(trace_on_off, "> Finished with hosts")
 
                 except Exception as e:
                     print("Error: ", e)
@@ -252,32 +250,35 @@ def main():
                 finally:
                     # print(browser.get_full_page_screenshot_as_base64())
                     # browser.get_full_page_screenshot_as_png()
-                    doTraceLog(trace_on_off, "\nLogging off, exec JS-function...")
+                    doTraceLog(trace_on_off, "\n> Logging off, exec JS-function...")
+                    doTraceLog(trace_on_off, "> browser page is: " + browser.current_url)
                     # browser.get(LOGOUT_URL) - not working, need 'special' POST-request
                     doLogOut_post_rq(browser, path=LOGOUT_PATH, params={})
-                    doTraceLog(trace_on_off, "browser page is: " + browser.current_url)
-                    doTraceLog(trace_on_off, "Waiting...")
+                    doTraceLog(trace_on_off, "> Waiting...")
                     attmpt = 0
                     while browser.title.startswith("My No-IP"):
-                        sleep(0.010)
-                        attmpt += 10
-                    doTraceLog(trace_on_off, "Awaited LogOut for: " + str(attmpt) + "ms")
-                    doTraceLog(trace_on_off, "browser page is: " + browser.current_url)
-                    doTraceLog(trace_on_off, "Changed BrowserTitle: " + browser.title)
-                    doTraceLog(trace_on_off, "NoIP logged out...")
+                        sleep(0.005)
+                        attmpt += 5
+                    sleep(0.020)
+                    doTraceLog(trace_on_off, "> Awaited LogOut for: " + str(attmpt) + "ms")
+                    doTraceLog(trace_on_off, "> now browser page is: " + browser.current_url)
+                    doTraceLog(trace_on_off, "> Changed BrowserTitle to: " + browser.title)
+                    doTraceLog(trace_on_off, "> NoIP-page logged out...")
 
         else:
             print("\nError: cannot login. Check if account is not blocked.")
             print(browser.current_url, browser.title)
-            print("NoIP - Log off\n")
+            print("NoIP - switch to Log off\n")
             browser.get(LOGOUT_URL)
     else:
         print("Cannot access login page:\t" + LOGIN_URL)
         print("\nProvided browser page is:\t" + browser.current_url)
 
-    doTraceLog(trace_on_off, "Close Browser...")
+    time_bak = showTimeDiff(trace_on_off, time_bak)
+    doTraceLog(trace_on_off, "> Closing Browser...")
     browser.quit()
-    doTraceLog(trace_on_off, "Browser quit.")
+    time_bak = showTimeDiff(trace_on_off, time_bak)
+    doTraceLog(trace_on_off, "> Browser closed.")
 
 
 if __name__ == "__main__":
